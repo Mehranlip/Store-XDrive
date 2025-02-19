b8092b8aa346fa6151c0501be0db1216c6c293b6
Mehran <realmehranasadi@gamil.com>
2025-02-19 17:03:58 +0330

    fix position signIn signUp

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,11 +18,29 @@ function layout({ children }: { children: React.ReactNode }) {
             <p className="body-1">
               This is a place where you can store all your documents.
             </p>
-            <Image src="/assets/images/files.png" alt="files" width={342} height={342} className="transition-all hover:rotate-2 hover:scale-105"/>
+            <Image
+              src="/assets/images/files.png"
+              alt="files"
+              width={342}
+              height={342}
+              className="transition-all hover:rotate-2 hover:scale-105"
+            />
           </div>
         </div>
       </section>
-      {children}
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0 ">
+        <div className="mb-16 lg:hidden">
+          <Image
+            className="h-auto w-[200px] lg:w-[250px]"
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 }
