63607fc72b81e76588116188b9009988a4dcebb8
Mehran <realmehranasadi@gamil.com>
2025-02-20 16:19:35 +0330

    steup backend

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "next/image";
 
-function layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
-      <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+      <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
         <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
             src="/assets/icons/logo-full.svg"
@@ -13,36 +13,38 @@ function layout({ children }: { children: React.ReactNode }) {
             height={82}
             className="h-auto"
           />
+
           <div className="space-y-5 text-white">
-            <h1 className="h1">Manage your file the best way</h1>
+            <h1 className="h1">Manage your files the best way</h1>
             <p className="body-1">
               This is a place where you can store all your documents.
             </p>
-            <Image
-              src="/assets/images/files.png"
-              alt="files"
-              width={342}
-              height={342}
-              className="transition-all hover:rotate-2 hover:scale-105"
-            />
           </div>
+          <Image
+            src="/assets/images/files.png"
+            alt="Files"
+            width={342}
+            height={342}
+            className="transition-all hover:rotate-2 hover:scale-105"
+          />
         </div>
       </section>
 
-      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0 ">
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            className="h-auto w-[200px] lg:w-[250px]"
             src="/assets/icons/logo-full-brand.svg"
             alt="logo"
             width={224}
             height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
           />
         </div>
+
         {children}
       </section>
     </div>
   );
-}
+};
 
-export default layout;
+export default Layout;
