a86b3b367a7de9878b1628c4797f5bf926a6250b
Mehran <realmehranasadi@gmail.com>
2025-02-28 20:47:35 +0330

    end auth prosses

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,7 +16,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { createAccount } from "@/lib/actions/user.actions";
+import { createAccount, signInUser } from "@/lib/actions/user.actions";
 import OtpModal from "./OTPModal";
 
 
@@ -50,17 +50,20 @@ function AuthForm({ type }: { type: FormType }) {
     setIsLoading(true)
     setErrorMessage("")
 
-    try{
-      const user = await createAccount({
-        fullName: values.fullName || "",
-        email: values.email
-      })
-  
-      setAccountId(user.accountId)
+    try {
+      const user =
+        type === "sign-up"
+          ? await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
+
+      setAccountId(user.accountId);
     } catch {
-      setErrorMessage("Failed to create account")
+      setErrorMessage("Failed to create account. Please try again.");
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
 
   };
